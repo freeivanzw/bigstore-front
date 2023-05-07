267e99cdd6541f2cdfc47fb2f75231f31e970adb
Ivan Kozak <freeivanzw@gmail.com>
2023-05-07 23:51:34 +0300

    12341234

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/bigstore-front/build">
     <Provider store={store}>
       <App />
     </Provider>
