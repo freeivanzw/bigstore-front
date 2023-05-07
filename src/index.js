27dd7af0245f83430b52d464430ff0319f32d9a1
Ivan Kozak <freeivanzw@gmail.com>
2023-05-07 23:18:21 +0300

    asdfasdf

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, HashRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {store} from './store/index'
 import './index.css';
@@ -11,10 +11,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
